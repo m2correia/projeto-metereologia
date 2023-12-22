96877f32ee8b53934fbbd3c8b92beeee236437c7
m2correia <mara2correia@gmail.com>
2023-12-22 23:25:50 +0000

    Added the search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temp-value");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "06d63a7b564395b6fb5f4fo44079td20";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unites=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Peterborough");
