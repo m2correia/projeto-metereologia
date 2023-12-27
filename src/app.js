f2e769d76d371cbb42d5330dc0d15fab3e7b87fb
m2correia <mara2correia@gmail.com>
2023-12-27 21:53:11 +0000

    Injected the forecast html into js

--- src/app.js
+++ src/app.js
@@ -49,7 +49,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thurs", "Fri"];
+  let forecastHtml = " ";
+
+  days.forEach(function (day) {
+    forecastHtml += `
+    <div class="weather-forecast-date">
+      ${day}
+    </div>
+      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
+      alt="" width="42" />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+    </div>
+  `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Peterborough");
+displayForecast();
